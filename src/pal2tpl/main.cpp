127cc18e0b7b61f1bd9bb72388903a0f3fd7bee5
Thomas Amendt <tgamendt@gmail.com>
2020-06-04 18:48:28 +0100

    Fixed bugs in the palette conversion routines

--- src/pal2tpl/main.cpp
+++ src/pal2tpl/main.cpp
@@ -15,8 +15,8 @@
 
 const char* TPLPAL_MAGIC = "TPL";
 
-uint32_t GREY = 0xCCC;
-uint32_t BLACK = 0xD8D8D8;
+uint32_t GREY = 0xD8D8D8;
+uint32_t BLACK = 0x000000;
 
 void Gen2Tpl(uint8_t* tpl, const uint8_t* gen)
 {
@@ -52,7 +52,7 @@ int main(int argc, char** argv)
 	{
 		TCLAP::CmdLine cmd("Utility for converting Genesis / Landstalker palettes to/from Tile Layer Pro format.\n"
 			"Part of the landstalker_tools set: github.com/lordmir/landstalker_tools\n"
-			" - Written by LordMir, May 2020",
+			" - Written by LordMir, June 2020",
 			' ', "0.1");
 
 		TCLAP::UnlabeledValueArg<std::string> fileIn("input_file", "The input file (.pal/.tpl)", true, "", "in_filename");
@@ -60,6 +60,7 @@ int main(int argc, char** argv)
 		TCLAP::SwitchArg toTpl("t", "toTPL", "Convert input file from Genesis format to TPL format", false);
 		TCLAP::SwitchArg toGen("g", "toGen", "Convert input file from TPL format to Genesis format", false);
 		TCLAP::SwitchArg force("f", "force", "Force overwrite if file already exists and no offset has been set", false);
+		TCLAP::SwitchArg encodeLength("z", "encode_length", "Read/write palette length at start of file (GEN)", false);
 		TCLAP::ValueArg<uint32_t> start("s", "start", "The starting index into the palette", false, 0, "start_index");
 		TCLAP::ValueArg<uint32_t> length("l", "length", "The number of entries in the palette. Setting this parameter to zero will load as many entries as possible.", false, 0, "num_entries");
 		TCLAP::ValueArg<uint32_t> count("c", "count", "The total number of palettes to load", false, 1, "num_palettes");
@@ -71,6 +72,7 @@ int main(int argc, char** argv)
 			"size is greater than expected, then data could be overwritten!", false, 0, "offset");
 		cmd.xorAdd(toTpl, toGen);
 		cmd.add(force);
+		cmd.add(encodeLength);
 		cmd.add(fileIn);
 		cmd.add(fileOut);
 		cmd.add(inOffset);
@@ -82,10 +84,24 @@ int main(int argc, char** argv)
 		cmd.add(transparentColour);
 		cmd.parse(argc, argv);
 
-		size_t expected_entries = count.getValue() * length.getValue();
-		size_t expected_size = toTpl.getValue() ? (expected_entries * 2) : (16 * count.getValue() * 3 + strlen(TPLPAL_MAGIC) + 1);
-		size_t out_size = toTpl.getValue() ? (16 * count.getValue() * 3 + strlen(TPLPAL_MAGIC) + 1) : (expected_entries * 2);
+		// If we request n palettes containing m entries, then we expect to find m*n colours
+		size_t len = length.getValue();
+		size_t expected_entries = count.getValue() * len;
+		// If we are reading a Genesis type palette, then the size to read is simply double the number of colours (each colour is stored as 2 bytes)
+		size_t gen_pal_size = expected_entries * 2;
+		// If we are reading a TPL palette, then we work in blocks of 16 colours - expect a full 16 colour palette for each requested palette.
+		// A TPL palette uses 3 bytes per colour, and also contains a 4-character magic word at the start.
+		size_t tpl_pal_size = ((expected_entries + 15)/16) * 16 * 3 * count.getValue() + strlen(TPLPAL_MAGIC) + 1;
+		if (tpl_pal_size == strlen(TPLPAL_MAGIC) + 1)
+		{
+			tpl_pal_size += 16 * 3;
+		}
+		// Define our expected input and output sizes depending on the conversion direction
+		size_t expected_size = toTpl.getValue() ? gen_pal_size : tpl_pal_size;
+		size_t out_size = toTpl.getValue() ? tpl_pal_size : gen_pal_size;
 
+		// Setting length to zero tells the program to read in as many colours as possible. This will not work with the -c option, as the program will read
+		// in as many palettes as exist.
 		if (length.getValue() == 0)
 		{
 			if (count.getValue() > 1)
@@ -121,19 +137,44 @@ int main(int argc, char** argv)
 				throw std::runtime_error(msg.str());
 			}
 
+			if (toTpl.getValue() == true && encodeLength.isSet() == true)
+			{
+				if (filesize > 2)
+				{
+					uint16_t tmpw;
+					char tmpb;
+					ifs.read(&tmpb, 1);
+					tmpw = tmpb << 8;
+					ifs.read(&tmpb, 1);
+					tmpw |= tmpb;
+					expected_entries = (tmpw + 1);
+					expected_size = expected_entries * 2 + 2;
+					ifs.seekg(0, std::ios::beg);
+				}
+				else
+				{
+					throw std::runtime_error("Input file size is not big enough to contain all requested entries.");
+				}
+			}
+
 			if (filesize > expected_size && expected_size != 0)
 			{
-				std::cerr << "WARNING: Input file size is bigger than expected (" << expected_size << " bytes). Trailing bytes will be ignored." << std::endl;
+				std::cerr << "WARNING: Input file size (" << filesize << " bytes) is bigger than expected (" << expected_size << " bytes). Trailing bytes will be ignored." << std::endl;
 			}
 			else if (filesize < expected_size)
 			{
-				throw std::runtime_error("Input file size is not big enough to store all requested entries.");
+				throw std::runtime_error("Input file size is not big enough to contain all requested entries.");
 			}
 
 			input.reserve(static_cast<unsigned int>(filesize));
 			input.insert(input.begin(), std::istream_iterator<uint8_t>(ifs), std::istream_iterator<uint8_t>());
 		}
 		ifs.close();
+		// The expected entries being set to zero tells the program to read in as many colours as possible.
+		if (expected_entries == 0)
+		{
+			expected_entries = toTpl.getValue() ? (input.size() / 2) : ((input.size() - 4) / 3);
+		}
 
 		// Next, test our output file
 		std::ifstream outfile_in(fileOut.getValue(), std::ios::binary);
@@ -170,51 +211,60 @@ int main(int argc, char** argv)
 		outfile_in.close();
 
 		// Next, the conversion
-		outbuffer.assign(out_size, 0);
+		gen_pal_size = expected_entries * 2 + (encodeLength.isSet() ? 2 : 0);
+		tpl_pal_size = ((expected_entries + 15) / 16) * 16 * count.getValue() * 3 + strlen(TPLPAL_MAGIC) + 1;
+		expected_size = toTpl.getValue() ? gen_pal_size : tpl_pal_size;
+		out_size = toTpl.getValue() ? tpl_pal_size : gen_pal_size;
 		size_t outlen = 0;
 		size_t inlen = input.size() - inOffset.getValue();
+		size_t entries_per_pal = length.getValue() > 0 ? length.getValue() : expected_entries;
+		outbuffer.assign(out_size, 0);
 		
 		if (toTpl.isSet() == true)
 		{
-			size_t entries = (length.getValue() == 0) ? 16 - start.getValue() : length.getValue();
-			entries = std::min(input.size() / 2 / count.getValue(), entries);
-			memcpy(outbuffer.data(), TPLPAL_MAGIC, 4);
-			size_t offset = 4;
+			uint8_t* out = outbuffer.data();
 			uint8_t* in = input.data();
+			if (encodeLength.isSet() == true)
+			{
+				in += 2;
+			}
+			memcpy(out, TPLPAL_MAGIC, 4);
+			out += 4;
 			for(size_t p = 0; p < count.getValue(); ++p)
 			{
 				if (init.getValue())
 				{
-					rgb2Tpl(outbuffer.data() + offset, transparentColour.getValue());
-					rgb2Tpl(outbuffer.data() + offset + 1 * 3, GREY);
-					rgb2Tpl(outbuffer.data() + offset + 15 * 3, BLACK);
+					rgb2Tpl(out, transparentColour.getValue());
+					rgb2Tpl(out + 1 * 3, GREY);
+					rgb2Tpl(out + 15 * 3, BLACK);
 				}
 
-				offset += start.getValue() * 3;
-				for (size_t i = 0; i < entries; ++i)
+				for (size_t i = 0; i < entries_per_pal; ++i)
 				{
-					Gen2Tpl(outbuffer.data() + offset + (i + start.getValue()) * 3, in);
+					Gen2Tpl(out + (start.getValue() + i) * 3, in);
 					in += 2;
 				}
 
-				offset += 16 * 3;
+				out += 16 * 3;
 			}
 		}
 		else
 		{
-			size_t entries = (length.getValue() == 0) ? 16 - start.getValue() : length.getValue();
-			memcpy(outbuffer.data(), TPLPAL_MAGIC, 4);
-			size_t offset = 4;
+			uint8_t* out = outbuffer.data();
 			uint8_t* in = input.data() + 4;
+			if (encodeLength.isSet() == true)
+			{
+				out[0] = ((entries_per_pal - 1) >> 8) & 0xFF;
+				out[1] = (entries_per_pal - 1) & 0xFF;
+				out += 2;
+			}
 			for (size_t p = 0; p < count.getValue(); ++p)
 			{
-				offset += start.getValue() * 3;
-				for (size_t i = 0; i < entries; ++i)
+				for (size_t i = 0; i < entries_per_pal; ++i)
 				{
-					Tpl2Gen(outbuffer.data() + offset, in + (start.getValue() + i) * 3);
-					offset += 2;
+					Tpl2Gen(out, in + (start.getValue() + i) * 3);
+					out += 2;
 				}
-
 				in += 16 * 3;
 			}
 		}
